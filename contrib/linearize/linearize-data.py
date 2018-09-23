4937e9bcb230cb30ec3efd8354a7e3cb71516b7d
Gandalf86 <bitcoinx2@users.noreply.github.com>
2018-09-23 14:50:58 +0200

    Change network magic and genesis block

--- contrib/linearize/linearize-data.py
+++ contrib/linearize/linearize-data.py
@@ -283,9 +283,9 @@ if __name__ == '__main__':
 	settings['rev_hash_bytes'] = settings['rev_hash_bytes'].lower()
 
 	if 'netmagic' not in settings:
-		settings['netmagic'] = 'f9beb4d9'
+		settings['netmagic'] = '25174c22'
 	if 'genesis' not in settings:
-		settings['genesis'] = '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f'
+		settings['genesis'] = '0d1f81f54505fb35e17a78fbe1f99acbdb45a4346e8aaafbc7fa132e154a7146'
 	if 'input' not in settings:
 		settings['input'] = 'input'
 	if 'hashlist' not in settings:
