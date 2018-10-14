9a307556f5256342c586fe5694dbab9dfe79a441
Gandalf86 <bitcoinx2@users.noreply.github.com>
2018-10-14 17:48:43 +0200

    Disable SegWit deployment (BIP141, BIP143, BIP147)

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -105,10 +105,10 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 0;
         consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = 0;
 
-        // Deployment of SegWit (BIP141, BIP143, and BIP147)
+        // Deployment of SegWit (BIP141, BIP143, and BIP147) (Currently disabled)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1479168000; // November 15th, 2016.
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1510704000; // November 15th, 2017.
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 0;
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 0;
 
         // The best chain should have at least this much work.
         consensus.nMinimumChainWork = uint256S("0x000000000000000000000000000000000000000000f91c579d57cad4bc5278cc");
