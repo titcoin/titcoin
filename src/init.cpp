931cd0bf32e717682720f017c2c747aed6bc4dfc
Gandalf86 <bitcoinx2@users.noreply.github.com>
2018-10-15 23:12:23 +0200

    Replace selected URLs

--- src/init.cpp
+++ src/init.cpp
@@ -525,8 +525,8 @@ std::string HelpMessage(HelpMessageMode mode)
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/bitcoin/bitcoin>";
-    const std::string URL_WEBSITE = "<https://bitcoincore.org>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/titcoin/titcoin>";
+    const std::string URL_WEBSITE = "<https://titcoin.github.io>";
 
     return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2009, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
